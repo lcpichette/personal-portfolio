0942eb30fc9d171b06cb61668cd4586304d5b3bc
Lucas Pichette <lucascpichette@gmail.com>
2023-05-31 20:10:59 -0700

    improves about-me page

---
+++ website/src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+<section class="mx-auto px-8 shadow-lg pt-28 min-h-screen max-w-2xl">
+    <h1 class="text-4xl font-bold">About Lucas...</h1>
+    <p>You've likely already seen my resume, so here's some information that you <i>haven't</i> read yet.</p>
+    <h2 class="text-3xl font-bold pt-4">Professionally</h2>
+    <p class="text-gray-700 pb-2">
+        In college my ramen and education was funded by Amazon. I started as a Warehouse Associate, became an Area Manager, and then became a Data Analyst once people in the warehouse saw how useful automation can be!
+    </p>
+    <p class="text-gray-700 pb-2">
+        After graduating I joined Triad as a Full Stack Engineer, and have been there since. Throughout College and my time at Triad, I've poured my free time into launching my own company: Gold Fox Dev, a Digital Marketing Agency. I've had a few clients, but certainly nothing that puts a roof over my head.
+    </p>
+    <p>
+        In time I hope to create my own product that helps many in some niche way. Until then, I'll be doing my best to learn as much as I can and help others along the way.
+    </p>
+
+    <h2 class="text-3xl font-bold pt-4">Academically</h2>
+    <p class="text-gray-700 pb-2">While I got my B.S. in Computer Science from OSU, I feel as though most of the information I use today has come from "getting my hands dirty" with personal projects -- even moreso than my work experience.</p>
+
+    <h2 class="text-3xl font-bold pt-4">Personally</h2>
+    <p class="text-gray-700 pb-2">
+        Cat-dad and cloud enthusiast, I spend my free time tinkering with new serverless projects, watching TV, and going on hikes with friends.
+    </p>
+</section>
